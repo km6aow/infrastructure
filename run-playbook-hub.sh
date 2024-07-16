a53c21a4f0cfd3f70d0484f75761522848d35ccf
Mark Klein <mklein@dis.com>
2024-07-16 14:09:24 -0700

    Refactor docker installation

--- run-playbook-hub.sh
+++ run-playbook-hub.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-PLAYBOOK=${1-provision-host.yml}
+PLAYBOOK=${1-provision-docker.yml}
 export ANSIBLE_CONFIG=ansible-vm.cfg
 export ANSIBLE_PYTHON_INTERPRETER=/usr/bin/python3
 export LC_ALL="en_US.UTF-8"
 export LC_CTYPE="en_US.UTF-8"
 export CI=true
-ansible-playbook -v -u aredn \
+ansible-playbook -vvv -u ansible \
     --extra-vars="{ \
       \"ANSIBLE_GITHUB_TOKEN\": \"${ANSIBLE_GITHUB_TOKEN}\", \
       \"ANSIBLE_HUB_PASSWORD\": \"${ANSIBLE_HUB_PASSWORD}\", \
@@ -15,7 +15,7 @@ ansible-playbook -v -u aredn \
       \"CONCOURSE_VAULT_TOKEN\": \"${CONCOURSE_VAULT_TOKEN}\", \
       \"GRAFANA_ADMIN_PASSWORD\": \"${GRAFANA_ADMIN_PASSWORD}\" \
       }" \
-    --limit=ubuntu-vm.dis.com \
+    --limit=hub.dis.com \
     --private-key=ssh/ansible_rsa \
-    --inventory inventory/vm.yml \
+    --inventory inventory/hub.yml \
     ${PLAYBOOK}
