1f29c1f981402e2e6275ca55688d7b51246efcce
Mark Klein <mklein@dis.com>
2024-07-24 12:50:02 -0700

    First functional

--- run-playbook-hub.sh
+++ run-playbook-hub.sh
@@ -5,7 +5,7 @@ export ANSIBLE_PYTHON_INTERPRETER=/usr/bin/python3
 export LC_ALL="en_US.UTF-8"
 export LC_CTYPE="en_US.UTF-8"
 export CI=true
-ansible-playbook -vvv -u ansible \
+ansible-playbook -v -u ansible \
     --extra-vars="{ \
       \"ANSIBLE_GITHUB_TOKEN\": \"${ANSIBLE_GITHUB_TOKEN}\", \
       \"ANSIBLE_HUB_PASSWORD\": \"${ANSIBLE_HUB_PASSWORD}\", \
